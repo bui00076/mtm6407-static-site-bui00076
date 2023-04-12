2db14660ec5453361dabee3cbeb431c633f97638
bui00076 <bui00076@algonquinlive.com>
2023-04-11 22:06:50 -0400

    completes

--- pages/Gallery.js
+++ pages/Gallery.js
@@ -15,19 +15,19 @@ export default function Gallery() {
         <Header title="Gallery" />
         <div className="row">
             <div className="col-md-4">
-              <img className="img-fluid" src="https://i.pinimg.com/564x/cb/a3/32/cba33262a6f76b23ac1ec97831280e18.jpg" alt="Card image cap"   data-size="1600x1067"/>
+              <img className="img-fluid" src="https://i.pinimg.com/564x/39/86/cb/3986cb17f4e847a8b22dcd511965c1fb.jpg" alt="Card image cap" />
         </div>
         <div className="col-md-4">
-              <img className="img-fluid" src="https://i.pinimg.com/564x/c7/d9/0a/c7d90ae9dc45f9b17ab678c34a1f3e78.jpg" alt="Card image cap"data-size="1600x1067"/>
+              <img className="img-fluid" src="https://i.pinimg.com/564x/c7/d9/0a/c7d90ae9dc45f9b17ab678c34a1f3e78.jpg" alt="Card image cap"/>
         </div>
         <div className="col-md-4">
-              <img className="img-fluid" src="https://i.pinimg.com/564x/91/cb/c9/91cbc9a3e13116f1b3e1ba77a43de0a9.jpg" alt="Card image cap"data-size="1600x1067"/>
+              <img className="img-fluid" src="https://i.pinimg.com/736x/88/43/45/884345c39385019d60fe15f12ea74c34.jpg" alt="Card image cap"/>
         </div>
         <div className="col-md-4">
-              <img className="img-fluid" src="https://i.pinimg.com/564x/26/95/d2/2695d286bfe8e945eb627e30d4cf4d35.jpg" alt="Card image cap"data-size="1600x1067"/>
+              <img className="img-fluid" src="https://i.pinimg.com/564x/26/95/d2/2695d286bfe8e945eb627e30d4cf4d35.jpg" alt="Card image cap"d/>
         </div>
         <div className="col-md-4">
-              <img className="img-fluid" src="https://i.pinimg.com/736x/12/ca/10/12ca10e260cca299a17b17cd417d92b5.jpg" alt="Card image cap"data-size="1600x1067"/>
+              <img className="img-fluid" src="https://i.pinimg.com/736x/12/ca/10/12ca10e260cca299a17b17cd417d92b5.jpg" alt="Card image cap"/>
         </div>
         <div className="col-md-4">
               <img className="img-fluid" src="https://i.pinimg.com/736x/f2/cd/a7/f2cda7b01a9c5eb2f70fa77944300ae5.jpg" alt="Card image cap"data-size="1600x1067"/>
