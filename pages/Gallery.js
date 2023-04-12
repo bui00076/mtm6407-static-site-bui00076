59b6b5e6f42cabe0e237c53d9b19ac929837b671
bui00076 <bui00076@algonquinlive.com>
2023-04-11 20:13:08 -0400

    completes

--- pages/Gallery.js
+++ pages/Gallery.js
@@ -15,22 +15,22 @@ export default function Gallery() {
         <Header title="Gallery" />
         <div className="row">
             <div className="gallery">
-            <div className="mb-3">
+            <div className="col-md">
               <img className="img-fluid" src="https://i.pinimg.com/564x/cb/a3/32/cba33262a6f76b23ac1ec97831280e18.jpg" alt="Card image cap"/>
         </div>
-        <div className="mb-3">
+        <div className="col-md">
               <img className="img-fluid" src="https://i.pinimg.com/564x/c7/d9/0a/c7d90ae9dc45f9b17ab678c34a1f3e78.jpg" alt="Card image cap"/>
         </div>
-        <div className="mb-3">
+        <div className="col-md">
               <img className="img-fluid" src="https://i.pinimg.com/564x/91/cb/c9/91cbc9a3e13116f1b3e1ba77a43de0a9.jpg" alt="Card image cap"/>
         </div>
-        <div className="mb-3">
+        <div className="col-md">
               <img className="img-fluid" src="https://i.pinimg.com/564x/26/95/d2/2695d286bfe8e945eb627e30d4cf4d35.jpg" alt="Card image cap"/>
         </div>
-        <div className="mb-3">
+        <div className="col-md">
               <img className="img-fluid" src="https://i.pinimg.com/736x/12/ca/10/12ca10e260cca299a17b17cd417d92b5.jpg" alt="Card image cap"/>
         </div>
-        <div className="mb-3">
+        <div className="col-md">
               <img className="img-fluid" src="https://i.pinimg.com/736x/f2/cd/a7/f2cda7b01a9c5eb2f70fa77944300ae5.jpg" alt="Card image cap"/>
             </div>
         </div>
